6346bcec892f5ec8f166b62b60273215ac436b42
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-12-17 18:51:41 +0530

    Sean Allen explaination on Delegate Protocol started

---
+++ SwiftUI_4_Accessory/DelegtesProtocol/DelegtesProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  DelegtesProtocol.swift
+//  SwiftUI_4_Accessory
+//
+//  Created by MANAS VIJAYWARGIYA on 17/12/22.
+//
+
+import SwiftUI
+
+struct DelegtesProtocol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DelegtesProtocol_Previews: PreviewProvider {
+    static var previews: some View {
+        DelegtesProtocol()
+    }
+}
